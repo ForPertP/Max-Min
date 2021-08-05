6dcf17ae7c21d125a86bfe839a63448a7f3bb93a
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-08-06 08:44:43 +0900

    Create Max-Min.cpp

---
+++ Max-Min.cpp
@@ -0,0 +1,16 @@
+int maxMin(int k, vector<int> arr) 
+{
+    std::sort(arr.begin(),arr.end());
+    int n = arr.size();
+    int minUnfairness = INT_MAX;
+    
+    for (int i = 0; i <= n-k; ++i)
+    {
+        if ((arr[i+k-1]-arr[i]) < minUnfairness)
+        {
+            minUnfairness = arr[i+k-1] - arr[i];
+        }
+    }
+    
+    return minUnfairness;
+}
